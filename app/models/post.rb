7fcb76dae83c62db311d1a3bb5458f9a8ac00552
basem909 <babdelrahman@blink.global>
2024-05-13 04:37:46 -0300

    Feature: Add user and post validations and relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
+  include StringColumnsValidation
+
+  # Relations
   belongs_to :user
+  has_many :ratings
+
+  # Validations
+  validate_short_string_columns :title, presence: true
+  validate_long_string_column :body, presence: true, maximum: 1000,  allow_blank: false
+  validates :ip, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   include StringColumnsValidation
 
+  # Relations
+  has_many :posts
+  has_many :ratings
+
   validate_short_string_columns login, presence: true, uniqueness: true, accept_special_char: true
 end
