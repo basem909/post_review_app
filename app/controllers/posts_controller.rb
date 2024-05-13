18aa4c0ada4d11e3d1b93479c367ebf1d32ccdc5
basem909 <basemshams30@gmail.com>
2024-05-13 07:03:46 -0300

    Feature: Add create endpoint in posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  before_action :set_user, only: [:create]
+
+  def create
+    post = @user.posts.new(post_params)
+
+    if post.save
+      render json: { message: "Post created successfully!", post: post, user: @user}, status: :ok
+    else
+      render json: { error: post.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(login: params[:login])
+    unless @user
+      render json: { error: "User's login must be present" }, status: :unprocessable_entity and return
+    end
+  end
+
+  def post_params
+    params.permit(:title, :body, :ip)
+  end
+end
