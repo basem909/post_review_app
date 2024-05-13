57f6452b4677b3bca526072dd7deab6e9e791b0d
basem909 <babdelrahman@blink.global>
2024-05-12 22:22:42 -0300

    Feature: Create posts table table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_004810) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_011934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "body", null: false
+    t.string "ip", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login", null: false
@@ -20,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_004810) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
