a9830eee23159f7811fe1bd322874e147e2f8c78
basem909 <basemshams30@gmail.com>
2024-05-13 05:53:38 -0300

    Feature: validate ratings attrs

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,6 +6,6 @@ class Rating < ApplicationRecord
   belongs_to :user
 
   # Validations
-  validates :rating, presence: true
+  validates :value, presence: true
   validates :value, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: 'must be an integer between 1 and 5' }
 end
